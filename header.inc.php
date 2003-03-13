747e2b1cc31f1ca150e2d31caf7bb9a5a66a8cda
robbat2 <robbat2>
2003-03-13 11:28:04 +0000

    Actually validate the login now

--- header.inc.php
+++ header.inc.php
@@ -1,5 +1,5 @@
 <?php
-    /* $Id: header.inc.php,v 1.3 2003/03/13 09:38:32 robbat2 Exp $ */
+    /* $Id: header.inc.php,v 1.4 2003/03/13 11:28:04 robbat2 Exp $ */
     include 'include.php';
 include './gui/header.inc.php';
 if (isset($skipvalidate) && $skipvalidate && !isset($_COOKIE['skipvalidate'])
@@ -7,7 +7,12 @@ if (isset($skipvalidate) && $skipvalidate && !isset($_COOKIE['skipvalidate'])
     $uservalid = TRUE;
     $validationskipped = TRUE;
 } else {
-    $uservalid = validateuser();
+    $uservalid = admin_validate();
+    if(!$uservalid) {
+        //possible hack or invalid password
+        header('Location: http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF'])."/".'index.php'.'?error=1');
+        exit;
+    }
     $validationskipped = FALSE;
 }
 

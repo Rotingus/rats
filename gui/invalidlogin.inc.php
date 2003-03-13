a43a4d39066280a9c10a4f708a65bfae43d26370
robbat2 <robbat2>
2003-03-13 11:53:17 +0000

    HTML/PHP cleanups

--- gui/invalidlogin.inc.php
+++ gui/invalidlogin.inc.php
@@ -1,9 +1,11 @@
 <?php
-/* $Id: invalidlogin.inc.php,v 1.1 2003/03/13 11:27:52 robbat2 Exp $ */
+/* $Id: invalidlogin.inc.php,v 1.2 2003/03/13 11:53:17 robbat2 Exp $ */
 /* $Source: /code/convert/cvsroot/infrastructure/rats/gui/invalidlogin.inc.php,v $ */
 ?>
-<h4>Invalid Login/Password</h4>
-(insert reason here)<br />
+<p class="title"
+<h4 class="title">Invalid Login/Password</h4>
+<h5 class="title">(insert reason here)</h5>
+</p>
 <?php
 /* vim: set ft=php expandtab shiftwidth=4 softtabstop=4 tabstop=4: */
 ?>

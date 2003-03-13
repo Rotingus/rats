abf6a17b4c74e7bdffd79542167ecc0db7b5e464
robbat2 <robbat2>
2003-03-13 11:53:45 +0000

    put error after login box

--- index.php
+++ index.php
@@ -1,19 +1,20 @@
 <?php
-/* $Id: index.php,v 1.7 2003/03/13 11:28:13 robbat2 Exp $ */
+/* $Id: index.php,v 1.8 2003/03/13 11:53:45 robbat2 Exp $ */
 /* vim: set ft=php expandtab shiftwidth=4 softtabstop=4 tabstop=4: */
 
 $skipvalidate = TRUE;
 
 include 'header.inc.php';
 include 'gui/title.inc.php';
+
+include 'gui/login.inc.php';
+
 if(isset($_GET['loginerror']) && $_GET['loginerror']) {
     $loginerror = TRUE;
     include 'gui/invalidlogin.inc.php';
 } else {
     $loginerror = FALSE;
 }
-
-include 'gui/login.inc.php';
 
 include 'footer.inc.php';
 ?>

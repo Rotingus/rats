7ae626100443c6456868cc095596c2af5a737fe2
robbat2 <robbat2>
2003-03-13 11:52:42 +0000

    header+footer fix

--- main.php
+++ main.php
@@ -3,14 +3,14 @@
 // either display error
 // or good results
 $frames = TRUE;
-include 'include.php';
-include 'gui/header.inc.php';
+include 'header.inc.php';
 ?>
 
 <frameset cols="96px,*" rows="*">
     <frame src="leftframe.php" name="nav" frameborder="0" />
     <frame src="rightframe.php" name="main" />
 </frameset>
+
 <?php
-include 'gui/footer.inc.php';
+include 'footer.inc.php';
 ?>

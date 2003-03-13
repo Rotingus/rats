68ce5811a75dd3f779e369f3d86e3c9806eb1392
robbat2 <robbat2>
2003-03-13 11:52:17 +0000

    bugfix in variable passing

--- header.inc.php
+++ header.inc.php
@@ -1,17 +1,18 @@
 <?php
-    /* $Id: header.inc.php,v 1.4 2003/03/13 11:28:04 robbat2 Exp $ */
-    include 'include.php';
+/* $Id: header.inc.php,v 1.5 2003/03/13 11:52:17 robbat2 Exp $ */
+include 'include.php';
 include './gui/header.inc.php';
 if (isset($skipvalidate) && $skipvalidate && !isset($_COOKIE['skipvalidate'])
-    && !isset($_GET['skipvalidate']) && !isset($_POST['skipvalidate'])) {
+        && !isset($_GET['skipvalidate']) && !isset($_POST['skipvalidate'])) {
     $uservalid = TRUE;
     $validationskipped = TRUE;
 } else {
     $uservalid = admin_validate();
     if(!$uservalid) {
         //possible hack or invalid password
-        header('Location: http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF'])."/".'index.php'.'?error=1');
+        header('Location: http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF'])."/".'index.php'.'?loginerror=1');
         exit;
+        //echo 'REDIRECT';
     }
     $validationskipped = FALSE;
 }

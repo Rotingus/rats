2fac0d57564304a61fe1259d075ddcf1966fdd6a
robbat2 <robbat2>
2003-03-13 11:27:15 +0000

    CVS tags

--- gui/title.inc.php
+++ gui/title.inc.php
@@ -1,4 +1,11 @@
+<?php
+/* $Id: title.inc.php,v 1.3 2003/03/13 11:27:15 robbat2 Exp $ */
+/* $Source: /code/convert/cvsroot/infrastructure/rats/gui/title.inc.php,v $ */
+?>
 <p class="title">
 <h1 class="title">RATS</h1>
 <h3 class="title">Research Asset Tracking System</h3>
 </p>
+<?
+/* vim: set ft=php expandtab shiftwidth=4 softtabstop=4 tabstop=4: */
+?>

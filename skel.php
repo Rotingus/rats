ead5321c03271e4793830fe59dbc5edbac864af8
robbat2 <robbat2>
2003-03-13 09:38:07 +0000

    Skeleton file for work

---
+++ skel.php
@@ -0,0 +1,8 @@
+<?php
+/* $Id: skel.php,v 1.1 2003/03/13 09:38:07 robbat2 Exp $ */
+/* $Source: /code/convert/cvsroot/infrastructure/rats/skel.php,v $ */
+
+// code goes here
+
+/* vim: set ft=php expandtab shiftwidth=4 softtabstop=4 tabstop=4: */
+?>

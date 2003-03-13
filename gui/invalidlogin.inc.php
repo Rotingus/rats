dff795bb95bc7514b3e52eddccc568acad346e56
robbat2 <robbat2>
2003-03-13 11:27:52 +0000

    Login error page

---
+++ gui/invalidlogin.inc.php
@@ -0,0 +1,9 @@
+<?php
+/* $Id: invalidlogin.inc.php,v 1.1 2003/03/13 11:27:52 robbat2 Exp $ */
+/* $Source: /code/convert/cvsroot/infrastructure/rats/gui/invalidlogin.inc.php,v $ */
+?>
+<h4>Invalid Login/Password</h4>
+(insert reason here)<br />
+<?php
+/* vim: set ft=php expandtab shiftwidth=4 softtabstop=4 tabstop=4: */
+?>

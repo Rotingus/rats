737560f06ca17f1d957eed7deeb3767c9d0b22a0
robbat2 <robbat2>
2003-03-14 23:10:11 +0000

    slight imrpoved

--- lib/table/_skel.php
+++ lib/table/_skel.php
@@ -1,5 +1,5 @@
 <?php
-/* $Id: _skel.php,v 1.1 2003/03/14 12:55:10 robbat2 Exp $ */
+/* $Id: _skel.php,v 1.2 2003/03/14 23:10:11 robbat2 Exp $ */
 /* $Source: /code/convert/cvsroot/infrastructure/rats/lib/table/_skel.php,v $ */
 
 //table name goes here
@@ -11,7 +11,7 @@ global $tableData;
 $tableData[$_tn]['field']['longname'] = '';
 $tableData[$_tn]['field']['datatype'] = '';
 $tableData[$_tn]['field']['inputtype'] = '';
-$tableData[$_tn]['field']['hidden'] = '';
+$tableData[$_tn]['field']['locked'] = '';
 $tableData[$_tn]['field']['hidden'] = '';
 
 /* vim: set ft=php expandtab shiftwidth=4 softtabstop=4 tabstop=4: */

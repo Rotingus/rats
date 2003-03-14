04244cc36e94813019a9d4875885140df6255b47
robbat2 <robbat2>
2003-03-14 12:56:01 +0000

    tbl viewing form

---
+++ view.php
@@ -0,0 +1,13 @@
+<?php
+/* $Id: view.php,v 1.1 2003/03/14 12:56:01 robbat2 Exp $ */
+/* $Source: /code/convert/cvsroot/infrastructure/rats/view.php,v $ */
+
+include 'lib/commontable.inc.php';
+
+if($allowedPerm) {
+    //TODO
+    echo 'display data here';
+}
+
+/* vim: set ft=php expandtab shiftwidth=4 softtabstop=4 tabstop=4: */
+?>

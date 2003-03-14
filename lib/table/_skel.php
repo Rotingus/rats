edc54f3c845d1bbe0661edd25fe69fd07fe84b21
robbat2 <robbat2>
2003-03-14 12:55:10 +0000

    skel file for table format data

---
+++ lib/table/_skel.php
@@ -0,0 +1,18 @@
+<?php
+/* $Id: _skel.php,v 1.1 2003/03/14 12:55:10 robbat2 Exp $ */
+/* $Source: /code/convert/cvsroot/infrastructure/rats/lib/table/_skel.php,v $ */
+
+//table name goes here
+$_tn  = '';
+
+
+global $tableData;
+
+$tableData[$_tn]['field']['longname'] = '';
+$tableData[$_tn]['field']['datatype'] = '';
+$tableData[$_tn]['field']['inputtype'] = '';
+$tableData[$_tn]['field']['hidden'] = '';
+$tableData[$_tn]['field']['hidden'] = '';
+
+/* vim: set ft=php expandtab shiftwidth=4 softtabstop=4 tabstop=4: */
+?>

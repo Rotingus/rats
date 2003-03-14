8487f4415a513fa22f4b2a168216c43eff897fef
robbat2 <robbat2>
2003-03-14 12:56:23 +0000

    fixes

--- leftframe.php
+++ leftframe.php
@@ -1,10 +1,25 @@
 <?php
-/* $Id: leftframe.php,v 1.3 2003/03/13 11:51:42 robbat2 Exp $ */
+/* $Id: leftframe.php,v 1.4 2003/03/14 12:56:23 robbat2 Exp $ */
 
 include('header.inc.php');
 
-function item($n) {
-  echo html_a('insert.php?table='.str_replace($n,' ',''),$n,'leftnavitem','target="main"')."\n";
+//codes are:
+// v = view
+// a = add
+// va = both
+function item($n,$type = 'v') {
+  $tbl = 'table='.str_replace(' ','',$n);
+  $viewfile = 'view.php';
+  $addfile = 'insert.php';
+  $target = 'target="main"';
+  $class = 'leftnavitem';
+  if(is_integer(strpos($type,'v')) || is_integer(strpos($type,'a'))) {
+      echo html_a($viewfile.'?'.$tbl,$n,$class,$target);
+  }
+  if(is_integer(strpos($type,'a'))) {
+      echo html_a($addfile.'?'.$tbl,'(Add)',$class,$target);
+  }
+  echo '<br />'."\n";
 }
 
 $a = array(
@@ -24,7 +39,7 @@ foreach($a as $i) {
 item($i);
 };
 
-echo html_a('transactions.php','Transactions','leftnavitem','target="_top"')."\n";
+echo html_a('transactions.php','Transactions','leftnavitem','target="_top"').'<br />'."\n";
 echo html_a('logout.php','Logout','leftnavitem','target="_top"')."\n";
 
 include 'footer.inc.php';

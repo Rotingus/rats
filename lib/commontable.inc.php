75791342b455593284c245e41e7d6356fd602702
robbat2 <robbat2>
2003-03-15 00:23:51 +0000

    bugfix

--- lib/commontable.inc.php
+++ lib/commontable.inc.php
@@ -1,17 +1,19 @@
 <?php
-/* $Id: commontable.inc.php,v 1.2 2003/03/14 12:56:53 robbat2 Exp $ */
+/* $Id: commontable.inc.php,v 1.3 2003/03/15 00:23:51 robbat2 Exp $ */
 
-$table = isset($_GET['table']) ? $_GET['table'] : '';
+global $tableName;
+
+$tableName = isset($_GET['table']) ? $_GET['table'] : '';
 
 $validPermissions = false;
 
 //check permissions
-if(!admin_haspermissions($table)) {
+if(!admin_haspermissions($tableName,$perm)) {
 //show an error here
 echo "include('./gui/permissiondenied.php')";
 } else {
 //ok, we are ok
-include('./lib/table/'.$table.'.php')
+include './lib/table/'.$tableName.'.php';
 $validPermissions = true;
 }
 
--- view.php
+++ view.php
@@ -1,13 +1,96 @@
 <?php
-/* $Id: view.php,v 1.1 2003/03/14 12:56:01 robbat2 Exp $ */
+/* $Id: view.php,v 1.2 2003/03/15 00:22:50 robbat2 Exp $ */
 /* $Source: /code/convert/cvsroot/infrastructure/rats/view.php,v $ */
+
+include './header.inc.php';
+
+$perm = 'view';
 
 include 'lib/commontable.inc.php';
 
-if($allowedPerm) {
-    //TODO
-    echo 'display data here';
+function drawTable_top($head) {
+    echo html_table_open('',array('border'=>'1','cellspacing'=>'1','cellpadding'=>'1'));
+    echo html_thead_open();
+    echo html_tr_open();
+    foreach($head as $h) {
+        echo html_th_open();
+        echo($h);
+        echo html_th_close();
+    }
+    echo html_tr_close();
+    echo html_thead_close();
+    echo '<br />'."\n";
 }
+
+function drawTable_bottom() {
+    echo html_table_close();
+}
+
+function drawTable_row($row) {
+    foreach($row as $r) {
+        echo html_td_open();
+        echo($r);
+        echo html_td_close();
+    }
+}
+
+function drawTable($head,$data) {
+    drawTable_top($head);
+    foreach($data as $row) {
+        echo html_tr_open();
+        drawTable_row($row);
+        echo html_tr_close();
+        echo '<br />'."\n";
+    }
+    drawTable_bottom();
+}
+
+function drawTableSQL($head,$query) {
+    global $_MySQL;
+    echo $query;
+    drawTable_top($head);
+    $_MySQL->restart();
+    $_MySQL->query($query);
+    while($row = $_MySQL->getRow()) {
+        echo html_tr_open();
+        drawTable_row($row);
+        echo html_tr_close();
+    }
+    drawTable_bottom();
+}
+
+function array2commasep($arr) {
+    $s = '';
+    for($i = 0; $i < count($arr); $i++) {
+        $s .= ' '.$arr[$i];
+        if($i+1 < count($arr)) {
+            $s .= ',';
+        }
+    }
+    return $s;
+}
+
+function array_subkey($arr,$k2) {
+    $newarr = array();
+    while(list($k1,$v1) = each($arr)) {
+        if(isset($v1[$k2])) {
+            $newarr[$k1] = $v1[$k2];
+        }
+    }
+    return $newarr;
+}
+
+if($validPermissions) {
+ global $tableData;
+ $query = $tableData[$tableName]['_view_sql'];
+ $arr_srch = array('__TABLE__','__COLUMNS__');
+ $arr_repl = array($tableName,array2commasep($tableData[$tableName]['_view_cols']));
+ $query = str_replace($arr_srch,$arr_repl,$query);
+ $head = array_subkey($tableData[$tableName],'longname');
+ drawTableSQL($head,$query);
+}
+
+include './footer.inc.php';
 
 /* vim: set ft=php expandtab shiftwidth=4 softtabstop=4 tabstop=4: */
 ?>

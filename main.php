fc48f26b3aaf7e29df2bd54eb151250d8e365039
robbat2 <robbat2>
2003-03-24 20:11:00 +0000

    Resized frame

--- main.php
+++ main.php
@@ -6,7 +6,7 @@ $frames = TRUE;
 include 'header.inc.php';
 ?>
 
-<frameset cols="96px,*" rows="*">
+<frameset cols="160px,*" rows="*">
     <frame src="leftframe.php" name="nav" frameborder="0" />
     <frame src="rightframe.php" name="main" />
 </frameset>

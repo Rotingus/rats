1196e48c4a80e129badca7f1a07eabdf738231c4
robbat2 <robbat2>
2003-05-01 22:08:39 +0000

    moved permissiondenied.php to permissiondenied.inc.php

---
+++ gui/permissiondenied.inc.php
@@ -0,0 +1,9 @@
+<?php
+/* $Id: permissiondenied.inc.php,v 1.1 2003/05/01 22:08:39 robbat2 Exp $ */
+/* $Source: /code/convert/cvsroot/infrastructure/rats/gui/permissiondenied.inc.php,v $ */
+
+// code goes here
+echo 'permissiondenied!';
+
+/* vim: set ft=php expandtab shiftwidth=4 softtabstop=4 tabstop=4: */
+?>

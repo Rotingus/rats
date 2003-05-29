2e9ef076e1957325838a1dbb0275ce6a22526044
robbat2 <robbat2>
2003-05-29 04:03:49 +0000

    wip

--- addedit.php
+++ addedit.php
@@ -1,5 +1,5 @@
 <?php
-/* $Id: addedit.php,v 1.2 2003/05/27 18:57:29 robbat2 Exp $ */
+/* $Id: addedit.php,v 1.3 2003/05/29 04:03:49 robbat2 Exp $ */
 /* $Source: /code/convert/cvsroot/infrastructure/rats/addedit.php,v $ */
 
 include './header.inc.php';
@@ -7,10 +7,16 @@ include './header.inc.php';
 $perm = 'add';
 
 include 'lib/commontable.inc.php';
-include 'gui/formbuilder.lib.php';
 
 if($tablePerm[$perm]) {
-    echo 'forms here';
+?>
+<form action="addedit.php" method="POST" class="dataform">
+<table class="dataform">
+<?php
+include 'gui/formbuilder.lib.php';
+?>
+</table></form>
+<?php
     //TODO
 } else {
     echo 'permission denied';

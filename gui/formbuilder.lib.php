39f9fcbf7f76a79301b22ab512941dd00ee66f9d
robbat2 <robbat2>
2003-06-03 19:50:40 +0000

    date stuff

--- gui/formbuilder.lib.php
+++ gui/formbuilder.lib.php
@@ -1,5 +1,5 @@
 <?php
-/* $Id: formbuilder.lib.php,v 1.2 2003/05/29 04:02:25 robbat2 Exp $ */
+/* $Id: formbuilder.lib.php,v 1.3 2003/06/03 19:50:40 robbat2 Exp $ */
 /* $Source: /code/convert/cvsroot/infrastructure/rats/gui/formbuilder.lib.php,v $ */
 
 function idstr_query($idfield,$idstr,$idfrom) {
@@ -26,7 +26,13 @@ function formelement_select($tableName,$tableData,$element,$data) {
     echo selectinput(fieldName($tableName,$element),$options,$v);
 }
 
-function formelement_dateselect($tableName,$tableData,$element,$data) { 
+function formelement_dateselect($tableName,$tableData,$element,$data) {
+    $item = $tableData[$tableName][$element];
+    $v = '';
+    if($data !== NULL) {
+        $v = $data[$element];
+    }
+    echo dateselect(fieldName($tableName,$element),$v);
 }
 
 function formelement_text($tableName,$tableData,$element,$data) { 
@@ -64,8 +70,57 @@ function formelement ($tableName,$tableData,$element,$data = NULL) {
     echo '</td></tr>';
 }
 
-?>
-<?php
+function seq($start,$increment='bad',$end='bad') {
+    if($end == 'bad' && $increment == 'bad') {
+        $end = $start;
+        $increment = 1;
+    } elseif ($end == 'bad') {
+        $end = $increment;
+        $increment = 1;
+    }
+    if($increment == 0) {
+        die('Not looking infinitely.');
+    }
+    $a = array();
+    for($i = $start; $i <= $end; $i += $increment) {
+        $a[] = $i;
+    }
+    return $a;
+}
+
+function dateselect($name,$val=0) {
+    if($val == 0 || $val == '' || $val === NULL) {
+        $val = time();
+    }
+    $months = array( 01=>'Jan', 02=>'Feb', 03=>'Mar', 04=>'Apr', 05=>'May', 06=>'Jun', 07=>'Jul', 08=>'Aug', 09=>'Sep', 10=>'Oct', 11=>'Nov', 12=>'Dec');
+
+    $arr = array(
+    'year' => array('dstr'=>'Y','str'=>'%.4d','low'=>1997,'high'=>2005,'prefix'=>'','suffix'=>'-'),
+    'month' => array('dstr'=>'m','str'=>'%.3s','low'=>0,'high'=>12,'index'=>$months,'prefix'=>'','suffix'=>'-'),
+    'day' => array('dstr'=>'d','str'=>'%.2d','low'=>0,'high'=>31,'prefix'=>'','suffix'=>' at '),
+    'hour' => array('dstr'=>'H','str'=>'%.2d','low'=>0,'high'=>23,'prefix'=>'','suffix'=>':'),
+    'minute' => array('dstr'=>'i','str'=>'%.2d','low'=>0,'high'=>59,'prefix'=>'','suffix'=>':'),
+    'second' => array('dstr'=>'s','str'=>'%.2d','low'=>0,'high'=>59,'prefix'=>'','suffix'=>'')
+    );
+    foreach($arr as $key=>$data) {
+        $arr[$key]['current'] = date($data['dstr'],$val);
+        $arr[$key]['values'] = seq($arr[$key]['low'],$arr[$key]['high']);
+    }
+    $str = '';
+    foreach($arr as $key=>$data) {
+        if(isset($data['index'])) {
+            $v = $data['index'];
+        } else {
+            $v = array();
+            foreach($data['values'] as $d) {
+                $v[$d] = sprintf($data['str'],$d);
+            }
+        }
+        $str .= $data['prefix'].selectinput(fieldName($name,$key),$v,$data['current']).$data['suffix'];
+    }
+    return $str;
+}
+
 foreach($tableData[$tableName]['_view_cols'] as $itemkey) {
     formelement($tableName,$tableData,$itemkey);
 }

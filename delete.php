44449591618a8f1589c54bfc03e9deaaf1880be8
robbat2 <robbat2>
2003-06-22 23:07:07 +0000

    rewrote delete script

--- delete.php
+++ delete.php
@@ -1,16 +1,24 @@
 <?php
-/* $Id: delete.php,v 1.1 2003/06/05 23:22:18 robbat2 Exp $ */
+/* $Id: delete.php,v 1.2 2003/06/22 23:07:07 robbat2 Exp $ */
 /* $Source: /code/convert/cvsroot/infrastructure/rats/delete.php,v $ */
 
 include './header.inc.php';
 
-$perm = 'add';
+$perm = v('perm','del');
 
 include 'lib/commontable.inc.php';
 
-if($validPermissions) {
-    echo 'forms here';
-    //TODO
+if($tablePerm[$perm]) {
+    $idDel = v('id');
+    $tableDel = v('table');
+    $query = 'DELETE FROM '.$tableName.' WHERE '.$tableData[$tableName]['_idkey'].'='.MySQL_quote($idDel);
+    echo $query;
+    $m = _MySQL_queryhelper($query);
+    $m->checkerror();
+    $_SESSION['msg'] = 'Item deleted from '.$tableName;
+    if(!dodbg()) { 
+        httpredirect('view.php?table='.$tableName);
+    }
 }
 
 /* vim: set ft=php expandtab shiftwidth=4 softtabstop=4 tabstop=4: */

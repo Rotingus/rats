0e4dc1c737533f12822b65d5dc22b467c64b8da3
robbat2 <robbat2>
2003-07-16 09:37:24 +0000

    move some output to debug mode. perm != mode

--- addedit.php
+++ addedit.php
@@ -1,5 +1,5 @@
 <?php
-/* $Id: addedit.php,v 1.5 2003/07/16 03:46:46 robbat2 Exp $ */
+/* $Id: addedit.php,v 1.6 2003/07/16 09:37:24 robbat2 Exp $ */
 /* $Source: /code/convert/cvsroot/infrastructure/rats/addedit.php,v $ */
 
 include './header.inc.php';
@@ -21,7 +21,7 @@ if($perm == 'edit') {
         die("Trying to edit a table $tableName without any ID key!\n");
     }
     $query = $tableData[$tableName]['_view_sql_all'].' WHERE '.$tableData[$tableName]['_idkey'].'='.MySQL_quote($idEdit);
-    echo "Query: $query\n";
+    if(dodbg()) echo 'Query: '.$query."<br />\n";
     global $MySQL_singleton_abort;
     $editData = MySQL_singletonassoc($query);
     if($editData === $MySQL_singleton_abort) {
@@ -30,7 +30,7 @@ if($perm == 'edit') {
     echo hiddeninput(fieldName($tableName,$tableData[$tableName]['_idkey']),$idEdit);
 }
 echo hiddeninput('table',$tableName);
-echo hiddeninput('mode',$perm);
+echo hiddeninput('perm',$perm);
 ?>
 <table class="dataform">
 <?php

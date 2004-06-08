5a4eabda208e29ec0ee10c26e940e283624760e7
mdeepwel <mdeepwel>
2004-06-08 15:16:40 +0000

    found useless function

--- footer.inc.php
+++ footer.inc.php
@@ -1,10 +1,10 @@
 <?php
-/* $Id: footer.inc.php,v 1.2 2003/06/05 23:12:24 robbat2 Exp $ */
+/* $Id: footer.inc.php,v 1.3 2004/06/08 15:16:40 mdeepwel Exp $ */
 /* $Source: /code/convert/cvsroot/infrastructure/rats/footer.inc.php,v $ */
 
 include 'gui/footer.inc.php';
 //code to flush page here
-ob_flush();
+ob_flush(); // this function exists in another dimension
 
 /* vim: set ft=php expandtab shiftwidth=4 softtabstop=4 tabstop=4: */
 ?>
